d797060daba1cd53ac50c734c7271e166f046f3b
ELATILLAH Mohamed <75954544+mohamedprojects@users.noreply.github.com>
2021-10-24 17:28:04 +0100

    Update about.php

--- Code/about.php
+++ Code/about.php
@@ -24,7 +24,6 @@ body {
 }
 
 
-/* Sticky navbar - toggles between relative and fixed, depending on the scroll position. It is positioned relative until a given offset position is met in the viewport - then it "sticks" in place (like position:fixed). The sticky value is not supported in IE or Edge 15 and earlier versions. However, for these versions the navbar will inherit default position */
 .navbar {
   overflow: hidden;
   background-color: #00b3b3;
@@ -51,7 +50,6 @@ body {
   color: black;
 }
 
-/* Active/current link */
 .navbar a.active {
   background-color: #666;
   color: white;
@@ -66,8 +64,6 @@ body {
   margin:60px;
 }
 
-/* Create two unequal columns that sits next to each other */
-/* Sidebar/left column */
 
 .hcp{
 	style:center;
@@ -82,7 +78,6 @@ body {
   padding: 20px;
 }
 
-/* Fake image, just for this example */
 .fakeimg {
   background-color: #aaa;
   width: 100%;
@@ -98,14 +93,12 @@ body {
   background: #00b3b3;
 }
 
-/* Responsive layout - when the screen is less than 700px wide, make the two columns stack on top of each other instead of next to each other */
 @media screen and (max-width: 700px) {
   .row {   
     flex-direction: column;
   }
 }
 
-/* Responsive layout - when the screen is less than 400px wide, make the navigation links stack on top of each other instead of next to each other */
 @media screen and (max-width: 400px) {
   .navbar a {
     float: none;
